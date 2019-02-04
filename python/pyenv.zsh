a371f938c2210e31b485e64a9f683f6f11ae83ab
Derek Michael Frank <derek@fanai.io>
2019-02-04 10:48:56 -0800

    chore(python): Fix Pyenv path alterations

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,7 +1,17 @@
-BIN="$HOME/.pyenv/bin"
-EXECUTABLE="$BIN/pyenv"
+export PYENV_ROOT="$HOME/.pyenv"
+PYENV_BIN="$PYENV_ROOT/bin"
+PYENV_SHIMS="$PYENV_ROOT/shims"
+
+if hash pyenv &>/dev/null; then
+    EXECUTABLE="$(which pyenv)"
+elif [[ -d "$PYENV_BIN" ]]; then
+    export PATH="$PYENV_BIN:$PATH"
+    EXECUTABLE="$PYENV_BIN/pyenv"
+fi
+
 if [[ -x "$EXECUTABLE" ]]; then
-    export PATH="$BIN:$PATH"
     eval "$($EXECUTABLE init -)"
     eval "$($EXECUTABLE virtualenv-init -)"
 fi
+
+export PATH
