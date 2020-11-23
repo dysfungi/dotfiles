5415fd2178597ed21e0f8e9eebcd5cd5f3bc5c1e
Derek M Frank <derek@frank.sh>
2020-11-22 16:14:58 -0800

    refactor(path): Use `uname -s` for checking OSes

--- go/path.zsh
+++ go/path.zsh
@@ -1,8 +1,9 @@
-if [[ "$(uname)" = 'Darwin' ]]; then
+if [[ "$(uname -s)" = Darwin ]]; then
     export GOROOT=/usr/local/opt/go/libexec
 else
     export GOROOT=/usr/local/go
 fi
+
 export GOPATH="$HOME/.local/gopath"
 export GOBIN="$GOPATH/bin"
 export PATH="$GOBIN:$GOROOT/bin:$PATH"
