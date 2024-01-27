c18e802f24beb3ed639fd2b50ec7c832ec6b96d6
Derek M Frank <dfrank@riotgames.com>
2024-01-26 16:36:18 -0800

    chore(riot): Update install of keyconjurer

--- riot/install.sh
+++ riot/install.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
 # TODO: make this a Brew tap? careful with security though
-keyconjurerBaseUrl="https://keyconjurer.security.riotgames.com/keyconjurer-darwin-"
-keyconjurerTarget="/usr/local/bin/keyconjurer"
-case "$(uname -m)" in
-    arm64)
-        curl -fsSl "$keyconjurerBaseUrl-amd64" "$keyconjurerTarget"
-        ;;
-    x86_64)
-        curl -fsSl "$keyconjurerBaseUrl-arm64" "$keyconjurerTarget"
-        ;;
-    *)
-        echo >&2 "Unsupported platform: cannot install keyconjurer"
-        ;;
-esac
+# https://keyconjurer.security.riotgames.com/
+if ! command -v keyconjurer >/dev/null; then
+    keyconjurerBaseUrl="https://keyconjurer.security.riotgames.com/keyconjurer-darwin"
+    keyconjurerTarget="/usr/local/bin/keyconjurer"
+    keyconjurerUrl=""
+    case "$(uname -m)" in
+        arm64)
+            keyconjurerUrl="$keyconjurerBaseUrl-arm64"
+            ;;
+        x86_64)
+            keyconjurerUrl="$keyconjurerBaseUrl-amd64"
+            ;;
+        *)
+            echo >&2 "Unsupported platform: cannot install keyconjurer"
+            ;;
+    esac
+    if [ -n "$keyconjurerUrl" ]; then
+        sudo wget --output-document="$keyconjurerTarget" "$keyconjurerUrl"
+        sudo chmod +x "$keyconjurerTarget"
+    fi
+fi
+keyconjurer login
