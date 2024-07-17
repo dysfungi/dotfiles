5db24e2f0b3dbacbb998279faa1977df193524a8
Derek M Frank <dfrank@riotgames.com>
2024-07-17 08:28:30 -0700

    chore(riot): Update alias

--- riot/aliases.zsh
+++ riot/aliases.zsh
@@ -1,2 +1,2 @@
 alias riotaws="keyconjurer login && keyconjurer get --ttl=8 --out=awscli --role=GL-Power product-services"
-alias riotvault="vault login -method=ldap username=${USER}"
+alias riotvault="vault login -method=oidc username=${USER}"
