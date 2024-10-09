0746161c50ac7f63eb34dcc03a9d073e4c0ecffa
Derek M Frank <dfrank@riotgames.com>
2024-10-09 15:40:36 -0700

    style(hammerspoon): Auto-indent init config

--- hammerspoon.symlink/init.lua
+++ hammerspoon.symlink/init.lua
@@ -1,36 +1,37 @@
+-- http://www.hammerspoon.org/docs/index.html
+
 -- wezterm configuration
 -- https://github.com/kovidgoyal/kitty/issues/45#issuecomment-1097554906
 hs.hotkey.bind({"ctrl"}, "space", function()
 
-  -- Get wezterm app
-  local app = hs.application.get("wezterm")
+    -- Get wezterm app
+    local app = hs.application.get("wezterm")
 
-  -- If app already open:
-  if app then
+    -- If app already open:
+    if app then
 
-      -- If no main window, then open a new window
-      if not app:mainWindow() then
-          app:selectMenuItem("New OS Window", true)
+        -- If no main window, then open a new window
+        if not app:mainWindow() then
+            app:selectMenuItem("New OS Window", true)
 
-      -- If app is already in front, then hide it
-      elseif app:isFrontmost() then
-          app:hide()
+        -- If app is already in front, then hide it
+        elseif app:isFrontmost() then
+            app:hide()
 
-      -- If there is a main window somewhere, bring it to current space and to front
-      else
-          -- Activate the app
-          app:activate()
-          -- Raise the main window and position correctly
-          app:mainWindow():raise()
-          app:mainWindow():moveToUnit('0.0,0.0,1.0,1.0')
-      end
+        -- If there is a main window somewhere, bring it to current space and to front
+        else
+            -- Activate the app
+            app:activate()
+            -- Raise the main window and position correctly
+            app:mainWindow():raise()
+            app:mainWindow():moveToUnit('0.0,0.0,1.0,1.0')
+        end
 
-  -- If app not open, open it
-  else
-      hs.application.launchOrFocus("wezterm")
-      app = hs.application.get("wezterm")
-  end
+    -- If app not open, open it
+    else
+        hs.application.launchOrFocus("wezterm")
+        app = hs.application.get("wezterm")
+    end
 
-  -- hs.spaces.gotoSpace(currentSpace)
-
+    -- hs.spaces.gotoSpace(currentSpace)
 end)
