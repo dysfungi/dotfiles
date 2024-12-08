edf1036f718f92c13a7ae213ccec4095fefa3535
Derek M Frank <derek@frank.sh>
2024-12-08 10:00:59 -0800

    style(homebrew): Apply auto-formatting

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,8 +1,8 @@
 # Brew install path depends on architecture: https://stackoverflow.com/a/71186857
 if [ -d "/opt/homebrew/bin" ]; then
-    brewPath="/opt/homebrew/bin"
+  brewPath="/opt/homebrew/bin"
 else
-    brewPath="/usr/local/bin"
+  brewPath="/usr/local/bin"
 fi
 
 eval "$($brewPath/brew shellenv)"
@@ -10,10 +10,10 @@ eval "$($brewPath/brew shellenv)"
 gnuPkgs=(coreutils grep gnu-sed)
 
 for gnuPkg in "${gnuPkgs[@]}"; do
-    gnuPkgBin="$(brew --prefix)/opt/$gnuPkg/libexec/gnubin"
-    if [ -d "$gnuPkgBin" ]; then
-        export PATH="$gnuPkgBin:$PATH"
-    else
-        echo >&2 "Cannot add GNU package to path: $gnuPkgBin"
-    fi
+  gnuPkgBin="$(brew --prefix)/opt/$gnuPkg/libexec/gnubin"
+  if [ -d "$gnuPkgBin" ]; then
+    export PATH="$gnuPkgBin:${PATH/:${gnuPkgBin}/}"
+  else
+    echo >&2 "Cannot add GNU package to path: $gnuPkgBin"
+  fi
 done
