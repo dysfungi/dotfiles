d85d243e883a4c284b77848a3ffe8eafefe966d8
Derek M Frank <derek@frank.sh>
2025-07-08 10:24:52 -0700

    chore!(wezterm): Migrate config and scripts to Chezmoi

--- wezterm/install.sh
+++
@@ -1,5 +0,0 @@
-# https://wezfurlong.org/wezterm/config/lua/config/term.html
-tempfile="$(mktemp)" \
-  && curl -o "$tempfile" https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
-  && tic -x -o "$HOME/.terminfo" "$tempfile" \
-  && rm "$tempfile"
